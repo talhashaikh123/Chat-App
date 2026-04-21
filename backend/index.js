80435d86d5fd0bda0ef61efe393a78ee3cd700ba
talhashaikh123 <shaikhtalha7943@gmail.com>
2026-04-21 15:15:58 +0530

    fix: remove frontend serving from backend

--- backend/index.js
+++ backend/index.js
@@ -35,12 +35,12 @@ app.use("/api/messages", messageRoutes);
 // Initialize socket AFTER creating server
 initSocket(server);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  app.get("*", (_, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-  });
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
+//   app.get("*", (_, res) => {
+//     res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+//   });
+// }
 
 server.listen(PORT, () => {
   console.log("Server running on port:", PORT);
