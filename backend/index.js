0ffe796ecd3af8a3ab668839d2005888c3d1fd32
Talha Shaikh <shaikhtalha7943@gmail.com>
2026-04-21 11:53:19 +0530

    added backend

--- backend/index.js
+++ backend/index.js
@@ -5,10 +5,10 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import path from "path";
 
-import { connectDB } from "./lib/db.js";
-import authRoutes from "./routes/auth.route.js";
-import messageRoutes from "./routes/message.route.js";
-import { initSocket } from "./lib/socket.js";
+import { connectDB } from "./src/lib/db.js";
+import authRoutes from "./src/routes/auth.route.js";
+import messageRoutes from "./src/routes/message.route.js";
+import { initSocket } from "./src/lib/socket.js";
 
 dotenv.config();
 
